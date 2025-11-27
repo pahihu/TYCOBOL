caebeedcc6888db4e8bade45d4843e200fbdce37
Andras Pahi <pahihu@gmail.com>
2025-11-27 22:00:25 +0100

    COBENV pulls in the COBOL compiler

--- env.sh
+++ env.sh
@@ -1,3 +1,9 @@
 DIR=$PWD
 export TYCOBOL=$DIR
 export PATH=$TYCOBOL/JCL:$PATH
+# macOS
+export COBENV=$HOME/bin/brew-env.sh
+# Rocky Linux 8
+# export COBENV=$HOME/bin/gc33db-env.sh
+# export COBENV=$HOME/bin/gc33vb-env.sh
+# export COBENV=$HOME/bin/gc33v-env.sh
