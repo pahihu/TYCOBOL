ab89434398834065f57c847fd193150883f9bc3a
Andras Pahi <pahihu@gmail.com>
2025-12-03 17:22:53 +0100

    UNSTRING, Camel-case, indexing, SET TO FALSE pp172

--- CBL/NMENTRY.cbl
+++ CBL/NMENTRY.cbl
@@ -7,11 +7,22 @@
        WORKING-STORAGE SECTION.
        01 WS-COUNTER           PIC 9(2)  VALUE ZEROS.
        01 WS-NUM-SPACES        PIC 9(2)  VALUE ZEROS.
+       01 FIELD-IDX            PIC 9     VALUE ZEROS.
+       01 FIELD OCCURS 3 TIMES PIC X(30) VALUE SPACES.
+       01 NUM-FIELDS           PIC 9     VALUE ZEROS.
+          88 LAST-NAME-ONLY    VALUE 1
+                               WHEN SET TO FALSE 4.
+          88 FIRST-AND-LAST    VALUE 2
+                               WHEN SET TO FALSE 5.
+          88 FIRST-LAST-MIDDLE VALUE 3
+                               WHEN SET TO FALSE 6.
       *---------------------BEGIN-PAN2SCR-------------------
        COPY WSSCRN.
        01 SC-NAME              PIC X(40) VALUE SPACES.
        01 SC-E-MAIL            PIC X(30) VALUE SPACES.
+       01 SC-NUM-FIELDS        PIC 9 VALUE ZEROS.
        01 SC-LAST              PIC X(30) VALUE SPACES.
+       01 SC-MIDDLE            PIC X(30) VALUE SPACES.
        01 SC-FIRST             PIC X(30) VALUE SPACES.
       *-----------------------------------------------------
        SCREEN SECTION.
@@ -19,24 +30,32 @@
           BLANK SCREEN, AUTO,
           FOREGROUND-COLOR IS 7,
           BACKGROUND-COLOR IS 1.
-          03 LINE 03 COLUMN 30 VALUE 'Name and E-mail Entry'
+          03 LINE 03 COLUMN 34 VALUE 'Name and E-mail Entry'
                                HIGHLIGHT.
-          03 LINE 06 COLUMN 05 VALUE 'Name:'.
-          03 LINE 06 COLUMN 11 PIC X(40)
+          03 LINE 06 COLUMN 09 VALUE 'Name:'.
+          03 LINE 06 COLUMN 15 PIC X(40)
                                REVERSE-VIDEO
                                REQUIRED
                                USING SC-NAME.
-          03 LINE 07 COLUMN 03 VALUE 'E-mail:'.
-          03 LINE 07 COLUMN 11 PIC X(30)
+          03 LINE 07 COLUMN 07 VALUE 'E-mail:'.
+          03 LINE 07 COLUMN 15 PIC X(30)
                                REVERSE-VIDEO
                                REQUIRED
                                USING SC-E-MAIL.
-          03 LINE 09 COLUMN 05 VALUE 'Last:'.
-          03 LINE 09 COLUMN 11 PIC X(30)
+          03 LINE 09 COLUMN 03 VALUE 'Num Fields:'.
+          03 LINE 09 COLUMN 15 PIC 9
+                               REVERSE-VIDEO
+                               FROM SC-NUM-FIELDS.
+          03 LINE 10 COLUMN 09 VALUE 'Last:'.
+          03 LINE 10 COLUMN 15 PIC X(30)
                                REVERSE-VIDEO
                                FROM SC-LAST.
-          03 LINE 10 COLUMN 04 VALUE 'First:'.
-          03 LINE 10 COLUMN 11 PIC X(30)
+          03 LINE 11 COLUMN 07 VALUE 'Middle:'.
+          03 LINE 11 COLUMN 15 PIC X(30)
+                               REVERSE-VIDEO
+                               FROM SC-MIDDLE.
+          03 LINE 12 COLUMN 08 VALUE 'First:'.
+          03 LINE 12 COLUMN 15 PIC X(30)
                                REVERSE-VIDEO
                                FROM SC-FIRST.
       *----------------------END-PAN2SCR--------------------
@@ -44,22 +63,43 @@
            DISPLAY NMENTRY-SCREEN
            ACCEPT NMENTRY-SCREEN
       * --- SPLIT NAME INTO LAST, FIRST --------------------
-           INSPECT SC-NAME
-               TALLYING WS-COUNTER
-               FOR CHARACTERS BEFORE INITIAL ','
-           MOVE SC-NAME(1:WS-COUNTER) TO SC-LAST
-      * --- MOVE AFTER COMMA -------------------------------
-           ADD 2 TO WS-COUNTER
-      * --- COUNT LEADING SPACES AFTER COMMA ---------------
-           INSPECT SC-NAME(WS-COUNTER:)
-               TALLYING WS-NUM-SPACES
-               FOR LEADING SPACES
-           MOVE SC-NAME(WS-COUNTER + WS-NUM-SPACES:)
-                TO SC-FIRST
+           UNSTRING SC-NAME DELIMITED BY ALL SPACE
+               INTO FIELD(1), FIELD(2), FIELD(3)
+               TALLYING IN NUM-FIELDS
+           END-UNSTRING
+           MOVE 1 TO FIELD-IDX
+           PERFORM CAMEL-CASE
+           MOVE 2 TO FIELD-IDX
+           PERFORM CAMEL-CASE
+           MOVE 3 TO FIELD-IDX
+           PERFORM CAMEL-CASE
+           IF LAST-NAME-ONLY
+               MOVE FIELD(1) TO SC-LAST
+           END-IF
+           IF FIRST-AND-LAST
+               MOVE FIELD(1) TO SC-FIRST
+               MOVE FIELD(2) TO SC-LAST
+           END-IF
+           IF FIRST-LAST-MIDDLE
+               MOVE FIELD(1) TO SC-FIRST
+               MOVE FIELD(2) TO SC-MIDDLE
+               MOVE FIELD(3) TO SC-LAST
+           END-IF
       * --- CONVERT E-MAIL TO LOWERCASE --------------------
            INSPECT SC-E-MAIL
                CONVERTING 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
                TO         'abcdefghijklmnopqrstuvwxyz'
+           SET LAST-NAME-ONLY TO FALSE
+           MOVE NUM-FIELDS TO SC-NUM-FIELDS
            DISPLAY NMENTRY-SCREEN
            STOP RUN
+           .
+      * ----------------------------------------------------
+       CAMEL-CASE.
+           INSPECT FIELD(FIELD-IDX)(1:1)
+               CONVERTING 'abcdefghijklmnopqrstuvwxyz'
+               TO         'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+           INSPECT FIELD(FIELD-IDX)(2:LENGTH OF FIELD - 1)
+               CONVERTING 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+               TO         'abcdefghijklmnopqrstuvwxyz'
            .
