785813d13214726b5fedb3223d51d1259b6cdcfd
Andras Pahi <pahihu@gmail.com>
2025-12-04 18:08:10 +0100

    added RULE Wolfram's 1D cellular automata

---
+++ CBL/RULE.cbl
@@ -0,0 +1,113 @@
+      ******************************************************************
+      *                                                                *
+      *             Wolfram's Rule N cellular automata                 *
+      *                                                                *
+      ******************************************************************
+000000*AA1ABBB+----2----+----3----+----4----+----5----+----6----+----7--SEQUENCE
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. RULE.
+       AUTHOR. Andras Pahi.
+       DATE-WRITTEN. 2025-12-04.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WORLD.
+          03 CELL        OCCURS 120 TIMES
+                         PIC X     VALUE SPACE.
+             88 ALIVE              VALUE '*'.
+       01 NEXT-WORLD.
+          03 NEXT-CELL   OCCURS 120 TIMES
+                         PIC X     VALUE SPACE.
+             88 NEXT-ALIVE         VALUE '*'.
+       01 POS            PIC 9(3)  BINARY VALUE ZERO.
+       01 HP             PIC 9(1)  BINARY VALUE ZERO.
+       01 GENERATION     PIC 9(3)  BINARY VALUE ZERO.
+       01 MAX-GENERATION PIC 9(3)  BINARY VALUE ZERO.
+       01 RULE           PIC 9(3)  BINARY VALUE ZERO.
+       01 ODD            PIC 9     BINARY VALUE ZERO.
+       01 RULE-PATTERN.
+          03 PATTERN     OCCURS 8 TIMES
+                         PIC 9     VALUE ZERO.
+             88 BORN               VALUE 1.
+       01 ARGC           PIC 9(3)  VALUE 0.
+       01 ARG            PIC X(80) VALUE SPACES.
+      *-----------------------------------------------------------------
+       PROCEDURE DIVISION.
+           PERFORM INIT-WORLD
+           DISPLAY WORLD
+           PERFORM VARYING GENERATION FROM 1 BY 1
+                   UNTIL GENERATION > MAX-GENERATION
+               PERFORM ALIVE-OR-DEAD
+               DISPLAY WORLD
+           END-PERFORM
+           GOBACK
+           .
+      *-----------------------------------------------------------------
+       INIT-WORLD.
+           COMPUTE MAX-GENERATION = LENGTH OF WORLD / 2
+           SET ALIVE(MAX-GENERATION) TO TRUE
+      * split RULE
+           PERFORM GET-RULE
+           PERFORM VARYING POS FROM 1 BY 1
+                   UNTIL POS > 8
+               COMPUTE ODD = FUNCTION REM(RULE, 2)
+               IF 1 = ODD
+                   SET BORN(POS) TO TRUE
+               END-IF
+               COMPUTE RULE = RULE / 2
+           END-PERFORM
+           .
+      *-----------------------------------------------------------------
+       ALIVE-OR-DEAD.
+           INITIALIZE NEXT-WORLD, HP
+      *-----------------------------------------------------------------
+      * Precompute cell(1) liveness, then computing the next requires
+      * only keeping the low 2 bits of HP and a shift left.
+      *-----------------------------------------------------------------
+           IF ALIVE(1)
+               ADD 2 TO HP
+           END-IF
+           IF ALIVE(2)
+               ADD 1 TO HP
+           END-IF
+           IF BORN(HP + 1)
+               SET NEXT-ALIVE(1) TO TRUE
+           END-IF
+           COMPUTE HP = 2 * HP
+      *-----------------------------------------------------------------
+           PERFORM VARYING POS FROM 2 BY 1
+                   UNTIL POS > LENGTH OF WORLD - 1
+               IF ALIVE(POS + 1)
+                   ADD 1 TO HP
+               END-IF
+               IF BORN(HP + 1)
+                   SET NEXT-ALIVE(POS) TO TRUE
+               END-IF
+               COMPUTE HP = 2 * FUNCTION REM(HP, 4)
+           END-PERFORM
+      *-----------------------------------------------------------------
+           IF BORN(HP + 1)
+               SET NEXT-ALIVE(LENGTH OF WORLD) TO TRUE
+           END-IF
+           MOVE NEXT-WORLD TO WORLD
+           .
+      *-----------------------------------------------------------------
+       GET-RULE.
+           ACCEPT ARGC FROM ARGUMENT-NUMBER
+           IF ARGC NOT = 1
+               DISPLAY 'ARGC=' ARGC
+               PERFORM SHOW-USAGE
+           END-IF
+           DISPLAY ARGC UPON ARGUMENT-NUMBER
+           ACCEPT  ARG  FROM ARGUMENT-VALUE
+           MOVE FUNCTION NUMVAL(ARG) TO RULE
+           IF RULE = 0
+               PERFORM SHOW-USAGE
+           END-IF
+           DISPLAY 'RULE ' ARG
+           .
+      *-----------------------------------------------------------------
+       SHOW-USAGE.
+           DISPLAY 'USAGE: RULE <RULE-NUMBER>'
+           MOVE 1000 TO RETURN-CODE
+           GOBACK
+           .
